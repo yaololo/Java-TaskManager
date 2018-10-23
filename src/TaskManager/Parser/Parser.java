641d456e2ef085682d12173fa76b1a060c869f76
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-23 08:06:26 +0800

    Create parse file format to user input method

--- src/TaskManager/Parser/Parser.java
+++ src/TaskManager/Parser/Parser.java
@@ -8,4 +8,17 @@ public class Parser {
         return fullCommand.split(" ")[0].toLowerCase();
     }
 
+//    public static String parseUserInputToFileFormat(String input ){
+//
+//    }
+
+    public static String parseFileFormatToUserInput(String line){
+        if(line.startsWith("T")){
+            return line.split("\\|")[0].trim() + " " + line.split("\\|")[2].trim();
+        }
+
+        return line.split("\\|")[0].trim() + " " + line.split("\\|")[2].trim()
+                + " / " + line.split("\\|")[3].trim();
+    }
+
 }
