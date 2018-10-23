a7167dd6be721ca48fdd159e132271595613b162
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-10-23 08:08:04 +0800

    Create DeadLine class

--- src/TaskManager/TaskList/Tasks/Deadline.java
+++ src/TaskManager/TaskList/Tasks/Deadline.java
@@ -1,4 +1,24 @@
 package TaskManager.TaskList.Tasks;
 
-public class Deadline {
+public class Deadline extends Task {
+    private String deadline;
+
+    public Deadline(String task, String deadline){
+        super(task);
+        this.setStatus(false);
+        setDeadline(deadline);
+    }
+
+    public void setDeadline(String deadline){
+        this.deadline = deadline;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+
+    public String getDetails(){
+        return getDescription() + "\n\tIt done? " + (getStatus()? "Yes" : "No") + "\n\t" + getDeadline();
+    }
 }
