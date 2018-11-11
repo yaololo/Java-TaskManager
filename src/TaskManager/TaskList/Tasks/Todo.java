35582af6faa368246d11f76a0c80420607ceea89
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:31:54 +0800

    Set taks type in Todo and get tasks type

--- src/TaskManager/TaskList/Tasks/Todo.java
+++ src/TaskManager/TaskList/Tasks/Todo.java
@@ -1,13 +1,18 @@
 package taskManager.taskList.Tasks;
 
 public class Todo extends Task {
+    private String type = "todo";
 
-    public Todo(String todo){
-        super(todo);
+    public Todo(String todo, int id){
+        super(todo, id);
     }
 
     public String getDetails(){
         return getDescription() + "\n\tIt done? " + (getStatus()? "Yes" : "No");
+    }
+
+    public String getTaskType(){
+        return type;
     }
 
 }
