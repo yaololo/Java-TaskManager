f8c61a576454bf472927a135bd1c882da2a82305
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:30:05 +0800

    Bug fix to make sure input date value is at fixed digits. eg. 1990 not 01900, and 1900-05-04 not 1900-5-4

--- src/TaskManager/inputEvaluator/InputValidator.java
+++ src/TaskManager/inputEvaluator/InputValidator.java
@@ -1,26 +1,14 @@
 package taskManager.inputEvaluator;
+import taskManager.exceptions.InvalidInputException;
 import taskManager.parser.Parser;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class InputValidator {
-    static final String ANSI_RESET = "\u001B[0m";
-    static final String ANSI_BLACK = "\u001B[30m";
-    static final String ANSI_RED = "\u001B[31m";
-    static final String ANSI_GREEN = "\u001B[32m";
-    static final String ANSI_YELLOW = "\u001B[33m";
-    static final String ANSI_BLUE = "\u001B[34m";
-    static final String ANSI_PURPLE = "\u001B[35m";
-    static final String ANSI_CYAN = "\u001B[36m";
-    static final String ANSI_WHITE = "\u001B[37m";
-
     static final int MAX_YEAR = 4000;
 
-    private String input;
-
-    public static void validateTodo(String todoTask) throws InvalidInputException{
+    public static void validateTodo(String todoTask) throws InvalidInputException {
         String description = new Parser().getTaskDescription(todoTask);
         if (description.isEmpty()){
             throw new InvalidInputException("Empty description for TODO");
@@ -48,19 +36,28 @@ public class InputValidator {
         }
     }
 
-    private static boolean validateDateOfDeadline(String inputDate){
-        SimpleDateFormat dataFormatValidator = new SimpleDateFormat("yyyy-MM-dd");
+    public static boolean validateDateOfDeadline(String inputDate){
+        String [] newArray = inputDate.split(" ");
+        String [] dateFormat = newArray[0].split("-");
+        String [] timeFormat = newArray[1].split(":");
+        SimpleDateFormat dataFormatValidator = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-        // @setLenient(false) is enforcing input to strictly match pattern e.g 2008-10-20 is valid, 2008/10/20 is invalid
-        dataFormatValidator.setLenient(false);
-        try {
-            //if not valid, it will throw ParseException
-            dataFormatValidator.parse(inputDate);
-        } catch (ParseException e) {
-            return false;
+        if(dateFormat[0].length() == 4 && dateFormat[1].length() == 2 && dateFormat[2].length() == 2 ){
+            if(timeFormat[0].length() == 2 && timeFormat[1].length() == 2 && timeFormat[2].length() == 2){
+
+                // @setLenient(false) is enforcing input to strictly match pattern e.g 2008-10-20 is valid, 2008/10/20 is invalid
+                dataFormatValidator.setLenient(false);
+
+                try {
+                    //if not valid, it will throw ParseException
+                    dataFormatValidator.parse(inputDate);
+                    return true;
+                } catch (ParseException e) {
+                    return false;
+                }
+            }
         }
-        return true;
 
+        return false;
     }
-
 }
