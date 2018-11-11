a85fcf025b339e693884c8a3e0936ebff03ec4f7
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:27:52 +0800

    Renaming variables in FileManager and  throws exceptions accordingly

--- src/TaskManager/FileManager/FileManager.java
+++ src/TaskManager/FileManager/FileManager.java
@@ -1,6 +1,6 @@
 package taskManager.fileManager;
+import taskManager.exceptions.InvalidInputException;
 import taskManager.taskList.TaskList;
-import taskManager.taskList.TaskManagerException;
 import taskManager.taskList.Tasks.Task;
 import taskManager.ui.Ui;
 import taskManager.parser.Parser;
@@ -19,7 +19,7 @@ public class FileManager {
         file = new File(path);
     }
 
-    public void loadFile(TaskList taskList){
+    public void loadFile(TaskList taskList) throws IOException{
         try {
             Scanner input = new Scanner(file);
 
@@ -31,11 +31,11 @@ public class FileManager {
             input.close();
 
         }catch (FileNotFoundException e){
-            new Ui().printError("problem encountered while opening file: " + e.getMessage());
+            throw new IOException("problem encountered while opening file: " + e.getMessage());
         }
     }
 
-    public void saveToFile(TaskList taskList){
+    public void saveToFile(TaskList taskList)throws IOException{
         try {
             PrintWriter output = new PrintWriter(file);
 
@@ -59,26 +59,31 @@ public class FileManager {
             output.close();
 
         }catch (IOException e){
-            new Ui().printError("problem encountered while writing data to file: " + e.getMessage());
+            throw new IOException("problem encountered while writing data to file: " + e.getMessage());
         }
     }
 
     private void addTask(String input, TaskList taskList){
+
         try {
             String formattedString = new Parser().parseFileFormatToUserInput(input);
 
             if (formattedString.startsWith("D")) {
 
                 taskList.addDeadline(formattedString);
+
             } else {
                 taskList.addTodo(formattedString);
             }
 
             if (Integer.parseInt(input.split("\\|")[1].trim()) == 1) {
-                taskList.getTaskList().get(taskList.getTaskList().size() -1).setStatus(true);
-        }
 
-        } catch (TaskManagerException e){
+                // @ get current task from the tasklist and set the status to true
+                taskList.getTaskList().get(taskList.getTaskList().size() -1).setStatus(true);
+            }
+
+        } catch (InvalidInputException e){
+
             new Ui().printError("Something wrong during adding task to taskList: " + e.getMessage());
         }
 
