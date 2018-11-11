ac850d8880f77989921ec3c4edf4fb569e848f1e
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:30:54 +0800

    Renaming variables and add get taskType method

--- src/TaskManager/TaskList/Tasks/Task.java
+++ src/TaskManager/TaskList/Tasks/Task.java
@@ -6,33 +6,29 @@ public abstract class Task {
     private String description;
     private Boolean status;
     private String createDate;
+    private int id;
 
-    public Task(String task){
-        addTask(task);
+    public Task(String task, int id){
+        setDescription(task);
         status = false;
-        createDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+        createDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        setId(id);
     }
 
-    public void addTask(String task){
-        description = task;
+    public void setDescription(String task){ description = task; }
 
-    }
-
-    public String getDescription(){
-        return description;
-    }
+    public String getDescription(){ return description; }
 
     public abstract String getDetails();
+    public abstract String getTaskType();
 
-    public void setStatus(boolean status){
-        this.status = status;
-    }
+    public void setStatus(boolean status){ this.status = status; }
 
-    public boolean getStatus() {
-        return this.status;
-    }
+    public boolean getStatus() { return this.status; }
 
-    public String getCreateDate(){
-        return createDate;
-    }
+    public int getId(){ return id; }
+
+    public void setId(int taskId){ id = taskId; }
+
+    public String getCreateDate(){ return createDate; }
 }
