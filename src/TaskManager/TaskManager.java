6ab5f765027332acda4113a032a1920b1b7238f1
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:26:49 +0800

    Avoid killing the program when there is an error thrown

--- src/TaskManager/TaskManager.java
+++ src/TaskManager/TaskManager.java
@@ -1,1 +1,73 @@
-package taskManager;import taskManager.parser.Parser;import taskManager.taskList.TaskList;import taskManager.taskList.TaskManagerException;import taskManager.ui.Ui;import taskManager.fileManager.FileManager;public class TaskManager {    private FileManager fileManager;    private TaskList tasks;    private Ui ui;    public TaskManager(String filePath){        ui = new Ui();        fileManager = new FileManager(filePath);        tasks = new TaskList();    }    public void run() {        ui.printWelcome();        fileManager.loadFile(tasks);//        ui.printMessage("Start loading tasks.");        boolean isExit = false;        while (!isExit) {            ui.printNumberOfTasks(tasks.getTotalNumberOfTask());            String userInputCommand = ui.readUserCommand();            String commandKeyWord = new Parser().getCommandWord(userInputCommand);            try {                switch (commandKeyWord) {                    case "exit":                    case "":                        isExit = true;                        break;                    case "todo":                        tasks.addTodo(userInputCommand);                        break;                    case "deadline":                        tasks.addDeadline(userInputCommand);                        break;                    case "print":                        ui.printTasks(tasks.getTaskList());                        break;                    case "done":                        tasks.markAsDone(userInputCommand);                        break;                    default:                        ui.printError("Unknown command! please try again ");                        break;                }            } catch (TaskManagerException e) {                System.out.println(e.getMessage());            }        }//        if()        fileManager.saveToFile(tasks);        ui.exit();    }}
+package taskManager;
+import taskManager.exceptions.InvalidInputException;
+import taskManager.parser.Parser;
+import taskManager.taskList.TaskList;
+import taskManager.ui.Ui;
+import taskManager.fileManager.FileManager;
+
+import java.io.IOException;
+
+public class TaskManager {
+
+    private FileManager fileManager;
+    private TaskList tasks;
+    private Ui ui;
+
+    public TaskManager(String filePath){
+        ui = new Ui();
+        fileManager = new FileManager(filePath);
+        tasks = new TaskList();
+    }
+
+    public void run() {
+        ui.printWelcome();
+        try {
+            fileManager.loadFile(tasks);
+            boolean isExit = false;
+
+            while (!isExit) {
+                try {
+                    ui.printNumberOfTasks(tasks.getTotalNumberOfTask(), tasks.getTotalNumberOfReminderTasks());
+
+                    String userInputCommand = ui.readUserCommand();
+                    String commandKeyWord = new Parser().getCommandWord(userInputCommand);
+
+                    switch (commandKeyWord) {
+                        case "exit":
+                        case "":
+                            isExit = true;
+                            break;
+                        case "command-help":
+                            ui.printCommands();
+                            break;
+                        case "date":
+//                        new InputEvaluator().deadlineDateEvluation(userInputCommand);
+                            break;
+                        case "todo":
+                            tasks.addTodo(userInputCommand);
+                            break;
+                        case "deadline":
+                            tasks.addDeadline(userInputCommand);
+                            break;
+                        case "print":
+                            ui.printALlTasks(tasks.getTaskList());
+                            break;
+                        case "done":
+                            tasks.markAsDone(userInputCommand);
+                            break;
+                        default:
+                            ui.printError("Unknown command! please try again ");
+                            break;
+                    }
+                } catch (InvalidInputException e) {
+                    ui.printError(e.getMessage());
+                }
+              }
+            fileManager.saveToFile(tasks);
+
+        } catch (IOException e) {
+            ui.printError(e.getMessage());
+        }
+        ui.exit();
+    }
+}
