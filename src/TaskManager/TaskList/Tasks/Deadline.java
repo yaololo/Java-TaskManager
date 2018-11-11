bc2959e930479bbf8a6081bc3204190c6383157c
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:35:29 +0800

    Create tasks type, check for if the tasks is overdue and set up reminderStatus and reminderTime

--- src/TaskManager/TaskList/Tasks/Deadline.java
+++ src/TaskManager/TaskList/Tasks/Deadline.java
@@ -1,32 +1,93 @@
 package taskManager.taskList.Tasks;
+import taskManager.parser.Parser;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
 
 public class Deadline extends Task {
     private String deadline;
-    private String dueDate;
+    private int timeToRemindInMin;
+    private boolean idOverDue;
+    private boolean reminderStatus;
+    private String type= "deadline";
+    private long timeBeforeDeadline;
 
-    public Deadline(String task, String deadline){
-        super(task);
+    public Deadline(String task, String deadline, int id){
+        super(task, id);
         this.setStatus(false);
         setDeadline(deadline);
+        timeToRemindInMin = 30;
+        updateReminderStatus();
+        reminderStatus= false;
+
     }
 
     public void setDeadline(String deadline){
         this.deadline = deadline;
     }
 
-    public String getDeadline() {
-        return deadline;
+    public void setTimeToRemindInMin(int newTime){
+        timeToRemindInMin = newTime;
+        updateReminderStatus();
     }
+
+    public String getDeadline() { return deadline; }
 
     public String getDetails(){
-        return getDescription() + "\n\tIt done? " + (getStatus()? "Yes" : "No") + "\n\t" + getDeadline();
+        return getDescription() + "\n\tIt done? " + (getStatus()? "Yes" : "No") + "\n\t" + getDeadline() + "\n\t"
+                + (idOverDue? "" : getDurationBeforeDeadline() + "until deadline\n\t") + "Current setting for reminder is "
+                + timeToRemindInMin + " minutes before deadline)";
     }
 
-    public void setDueDate() {
-        this.dueDate = dueDate;
+    private void updateReminderStatus(){
+        Date dueDate = new Parser().parseToDate(deadline);
+        //assert
+        Date now = new Date();
+        long diffInMilisecond = dueDate.getTime() - now.getTime();
+
+        if(diffInMilisecond <= 0){
+            System.out.println(diffInMilisecond);
+            idOverDue = true;
+            reminderStatus = false;
+        } else{
+            idOverDue = false;
+            long diffInMinute = TimeUnit.MILLISECONDS.toMinutes(diffInMilisecond);
+            timeBeforeDeadline = diffInMinute;
+
+            if(diffInMinute < timeToRemindInMin){
+                reminderStatus = true;
+            }
+            reminderStatus = false;
+        }
     }
 
-    public String getDueDate(){
-        return dueDate;
+    public boolean getReminderStatus(){ return reminderStatus; }
+
+    public String getTaskType(){ return type; }
+
+    public String getDurationBeforeDeadline(){
+        //assert timebeforedeadlin is not negative
+        long diffInMinutes = timeBeforeDeadline;
+
+        int years = (int)(diffInMinutes / 525600);
+        diffInMinutes %= 525600;
+        long days = (long) (diffInMinutes / 60 / 24);
+        diffInMinutes %= 1440;
+        long hours = (long) (diffInMinutes/ 60);
+        diffInMinutes %= 60;
+
+        String year, day, hour, min;
+
+        if(years == 0) year = "";
+        else year = String.valueOf(years) + " year(s) ";
+
+        if(days == 0 ) day = "";
+        else day = String.valueOf(days) + " day(s) ";
+
+        if(hours == 0) hour = "";
+        else hour = String.valueOf(hours) + " hour(s) ";
+
+        return year + day + hour + diffInMinutes + " minutes ";
+
     }
 }
