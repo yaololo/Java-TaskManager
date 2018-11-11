ddab513c8e108a41b6ab9dca0ab9517f3eb8b591
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-11 15:36:18 +0800

    Refactor code into a single line and create reminderList

--- src/TaskManager/TaskList/TaskList.java
+++ src/TaskManager/TaskList/TaskList.java
@@ -1,67 +1,63 @@
 package taskManager.taskList;
 import taskManager.inputEvaluator.InputValidator;
-import taskManager.inputEvaluator.InvalidInputException;
+import taskManager.exceptions.InvalidInputException;
+import taskManager.parser.Parser;
 import taskManager.taskList.Tasks.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
     private List<Task> tasks;
+    private List<Deadline> reminderList;
+    private int idCounter;
 
     public TaskList(){
         tasks = new ArrayList<>();
+        reminderList = new ArrayList<>();
+        idCounter= 1;
     }
 
-    public void addTodo(String line)throws InvalidInputException {
+    public void addTodo(String input)throws InvalidInputException {
         try {
-            new InputValidator().validateTodo(line);
-            tasks.add(new Todo(line.substring("todo".length()).trim()));
+            new InputValidator().validateTodo(input);
+            tasks.add(new Todo(input.substring("todo".length()).trim(), idCounter));
+            idCounter++;
 
         } catch (InvalidInputException e) {
             throw new InvalidInputException(e.getMessage());
-
         }
     }
 
-    public void addDeadline(String line) throws InvalidInputException {
+    public void addDeadline(String input) throws InvalidInputException {
         try {
-            new InputValidator().validateDeadline(line);
+            InputValidator.validateDeadline(input);
 
-            //
-            String task = line.substring("deadline".length(), line.indexOf('/')).trim();
-            String deadline = line.substring(line.indexOf('/')+ 1);
+            String taskDescription = Parser.getTaskDescription(input);
+            String deadline = new Parser().getTaskDeadline(input);
+            Deadline temp = new Deadline(taskDescription, deadline, idCounter);
+            tasks.add(temp);
+            idCounter++;
 
-            tasks.add(new Deadline(task, deadline));
+            if(temp.getReminderStatus()){
+//                reminderList.add(tasks.get(tasks.size()-1));
+            }
+
         } catch (InvalidInputException e){
             throw new InvalidInputException(e.getMessage());
         }
-//        if (!line.contains("/") || line.substring(line.indexOf("/")+1).trim().isEmpty()){
-//            throw new TaskManagerException("Empty deadline for Deadline task");
-//        }
-//
-//        String task = line.substring("deadline".length(), line.indexOf('/')).trim();
-//
-//        if (task.isEmpty()){
-//            throw new TaskManagerException("Empty description for TODO");
-//        }
-
-        String deadline = line.substring(line.indexOf('/')+ 1);
-
-//        tasks.add(new Deadline(task, deadline));
     }
 
     public void markAsDone(String line){
         int index = Integer.parseInt(line.substring("done".length()).trim());
         tasks.get(index - 1).setStatus(true);
-//        System.out.println("Tasks in the list: " + tasks.size());
     }
 
-    public int getTotalNumberOfTask(){
-        return tasks.size();
-    }
+    public int getTotalNumberOfTask(){ return tasks.size(); }
 
-    public List<Task> getTaskList(){
-        return tasks;
-    }
+    public List<Task> getTaskList(){ return tasks; }
+
+    public List<Deadline> getReminderList(){ return reminderList;}
+
+    public int getTotalNumberOfReminderTasks(){ return reminderList.size(); }
 
 }
