4c8fa2a1684be3755425218f531689ac89ae965e
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-13 14:54:00 +0800

    extract InvalidInputException into another file

--- src/TaskManager/exceptions/InvalidInputException.java
+++ src/TaskManager/exceptions/InvalidInputException.java
@@ -1,8 +1,7 @@
-package taskManager.inputEvaluator;
+package taskManager.exceptions;
 
 public class InvalidInputException extends Exception{
     public InvalidInputException(String message) {
         super(message);
     }
-
 }
