3b60132e70ff338dd07a4a82e447763295f3c36e
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-13 14:49:17 +0800

    Bug fix in getTaskDeadline method

--- src/TaskManager/Parser/Parser.java
+++ src/TaskManager/Parser/Parser.java
@@ -1,14 +1,9 @@
 package taskManager.parser;
-import taskManager.exceptions.InvalidInputException;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Calendar;
 
 public class Parser {
-
-    public Parser(){}
 
     public static String getCommandWord(String fullCommand){
         return fullCommand.split(" ")[0].toLowerCase();
@@ -28,7 +23,7 @@ public class Parser {
         String description;
 
         if (keyWord.startsWith("d")) {
-            description = task.substring("deadline".length(), task.indexOf('/')).trim();
+            description = task.substring("deadline".length(), task.lastIndexOf('/')).trim();
 
         } else {
             description = task.substring("todo".length()).trim();
@@ -41,7 +36,7 @@ public class Parser {
     }
 
     public static String getTaskDeadline(String task){
-        String dateOfDeadline = task.substring(task.indexOf("/")+1).trim();
+        String dateOfDeadline = task.substring(task.lastIndexOf("/")+1).trim();
         return dateOfDeadline;
     }
 
