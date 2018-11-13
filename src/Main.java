404190e8f631e7434e40963fb53d4f1551df9ef0
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-13 14:54:25 +0800

    Remove redundant Lines

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
-import TaskManager.TaskManager;
+import taskManager.TaskManager;
 
 public class Main {
 
     public static void main(String[] args) {
-        new TaskManager("/Users/yongming/personal/school/java_se/TaskManager/src/TaskManager/data/tasks.txt").run();
-
+//        assert args.length>0 : "No arguments";
+        new TaskManager("/Users/yongming/personal/school/java_se/taskManager/src/taskManager/data/tasks.txt").run();
     }
-
-//    private static String getInput() {
-//        System.out.printf("Number of task you have is %d, please enter your task: \n", tasks.size());
-//        return in.nextLine();
-//    }
 }
