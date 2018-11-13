0255f57753ea1e5defc97986854fb64a3bdee470
zhuangyongming <yongming.zhuang@moneysmart.com>
2018-11-14 03:56:10 +0800

    Use relative path

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 //        assert args.length>0 : "No arguments";
-        new TaskManager("/Users/yongming/personal/school/java_se/taskManager/src/taskManager/data/tasks.txt").run();
+        new TaskManager("./src/taskManager/data/tasks.txt").run();
     }
 }
